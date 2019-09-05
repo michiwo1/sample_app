69bd84c8b242962951f1d0098627cfee6238088a
EC2 Default User <ec2-user@ip-172-31-41-219.us-east-2.compute.internal>
2019-09-05 12:19:57 +0000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,9 @@ class UsersController < ApplicationController
   end
   
   def create
-   @user = User.new(user_params)
+  @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -18,11 +19,11 @@ class UsersController < ApplicationController
     end
   end
   
-   private
+  private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                                  :password_confirmation)
     end
   
 end
